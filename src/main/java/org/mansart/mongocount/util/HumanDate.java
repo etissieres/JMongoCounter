9e740e92673a9012a868d2236e58245385ed3415
etissieres <etienne.tissieres@gmail.com>
2014-03-14 18:30:15 +0100

    human date utils

---
+++ src/main/java/org/mansart/mongocount/util/HumanDate.java
@@ -0,0 +1,17 @@
+package org.mansart.mongocount.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HumanDate {
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("HH:mm:ss");
+
+    public static String format(long timestamp) {
+        return DATE_FORMAT.format(new Date(timestamp));
+    }
+
+    public static String now() {
+        return HumanDate.format(System.currentTimeMillis());
+    }
+}
